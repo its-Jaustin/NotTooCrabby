7272ef0941a4f60832fd7334778d3ba70a67fc75
its-Jaustin <jacksoa2@mail.gvsu.edu>
2025-04-25 12:23:33 -0400

    project transfer p2

---
+++ Assets/EgyptMonsters/Scripts/DealDamageComponent.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DealDamageComponent : MonoBehaviour {
+
+    public GameObject hitFX;
+	void DealDamage() {
+        transform.parent.GetComponent<Eagle_Controller>().DealDamage(this);
+    }
+	
+
+}
